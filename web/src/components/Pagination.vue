1f2874781e1612b164cbdf7a4478f0eed5d78f7d
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-05-02 17:04:10 +0200

    add(web): codebase

---
+++ web/src/components/Pagination.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="movies"
+      :items-per-page="100000"
+      class="elevation-1"
+    ></v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    headers: [
+      { text: "Titre", value: "title" },
+      { text: "Catégorie", value: "category" },
+      { text: "Nombre de locations", value: "total_rental" }
+    ],
+    movies: []
+  }),
+  mounted() {
+    this.$http
+      .get("/movies", {
+        query: {
+          limit: 10000
+        }
+      })
+      .then(res => (this.movies = res.data));
+  }
+};
+</script>
